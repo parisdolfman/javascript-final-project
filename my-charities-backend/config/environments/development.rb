e57ffc5990d62afaec3a81f45600c7f5c6198dff
parisdolfman <parisdolfman@gmail.com>
2021-05-19 13:58:23 -0400

    Forgot to use --api flag in generation; changed settings for app to function as API

--- my-charities-backend/config/environments/development.rb
+++ my-charities-backend/config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #Render an HTML page w/ debugging information
+  config.debug_exception_response_format = :defauly
 end
