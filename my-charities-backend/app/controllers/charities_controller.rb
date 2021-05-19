8f41bf599153c8b33f2e65127e7158d8fe0f7a28
parisdolfman <parisdolfman@gmail.com>
2021-05-19 13:43:57 -0400

    Stubbed out controllers

--- my-charities-backend/app/controllers/charities_controller.rb
+++ my-charities-backend/app/controllers/charities_controller.rb
@@ -1,2 +1,17 @@
 class CharitiesController < ApplicationController
+
+    def index
+        charities = Charity.all
+    end 
+
+    def show
+      charity = Charity.find_by(id: params[:id])
+    end 
+
+    private
+
+    def charity_params
+        params.require(:charity).permit(:name, :type, :image)
+    end 
+
 end
--- my-charities-backend/app/controllers/users_controller.rb
+++ my-charities-backend/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(id: params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end 
 end
