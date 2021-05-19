f6864c4557449b12ec669e9a5058bd20e1e51a43
parisdolfman <parisdolfman@gmail.com>
2021-05-19 16:16:23 -0400

    Adding in fns

--- my-charities-frontend/src/index.js
+++ my-charities-frontend/src/index.js
@@ -7,20 +7,41 @@ let glyphStates = {
     "♥": "♡"
   };
 
-  let colorStates = {
+let colorStates = {
     "blue" : "",
     "": "blue"
-  };
+};
 
-  let articleHearts = document.querySelectorAll(".like-glyph");
+let articleHearts = document.querySelectorAll(".like-glyph");
 
 
-  function likeCallback(e) {
+function likeCallback(e) {
     let heart = e.target;
     heart.innerText = glyphStates[heart.innerText];
     heart.style.color = colorStates[heart.style.color];
-  }
+}
 
-  for (let glyph of articleHearts) {
+for (let glyph of articleHearts) {
     glyph.addEventListener("click", likeCallback);
-  }
+}
+
+
+function fetchCharities(){
+    fetch(CHARITIES_URL)
+    .then(res => res.json())
+    .then(charities => renderCharities(charities))     
+}
+
+function renderCharities(charities){
+    mainContainer.innerHTML = ""
+    charities.forEach(charity => {
+        mainContainer.innerHTML += `<div class="card">
+        <img src=${charity.image} class="charity-avatar" />
+        <h2>${charity.name}</h2>
+        <p><em>${charity.category}</em></p> 
+        
+        <button onClick=addToList(event) data-charity-id="${charity.id}"> Add To List </button>
+        </br>
+      </div>`
+    })
+}
