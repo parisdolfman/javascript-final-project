1c357296a6e3f163a8c0f688beb2acc2b7427414
parisdolfman <parisdolfman@gmail.com>
2021-05-19 15:52:31 -0400

    Added styling

--- my-charities-frontend/src/index.js
+++ my-charities-frontend/src/index.js
@@ -1,3 +1,26 @@
 const BASE_URL = "http://localhost:3000"
 const CHARITIES_URL = '${BASE_URL}/charities'
 const USERS_URL = '${BASE_URL}/users'
+
+let glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+
+  let colorStates = {
+    "blue" : "",
+    "": "blue"
+  };
+
+  let articleHearts = document.querySelectorAll(".like-glyph");
+
+
+  function likeCallback(e) {
+    let heart = e.target;
+    heart.innerText = glyphStates[heart.innerText];
+    heart.style.color = colorStates[heart.style.color];
+  }
+
+  for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+  }
