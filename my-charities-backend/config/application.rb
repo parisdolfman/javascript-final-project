e5a3d5f1a56dfc95dcf0c702d7daa34e6959d47d
parisdolfman <parisdolfman@gmail.com>
2021-05-19 14:03:07 -0400

    Added rack cors

--- my-charities-backend/config/application.rb
+++ my-charities-backend/config/application.rb
@@ -19,5 +19,15 @@ module MyCharitiesBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+      
   end
 end
