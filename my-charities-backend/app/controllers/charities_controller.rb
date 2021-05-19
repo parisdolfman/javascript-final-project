6d654bafeeb8879391d43b0eb1fd01def3dc504b
parisdolfman <parisdolfman@gmail.com>
2021-05-19 14:10:26 -0400

    Filled out controller methods

--- my-charities-backend/app/controllers/charities_controller.rb
+++ my-charities-backend/app/controllers/charities_controller.rb
@@ -2,10 +2,16 @@ class CharitiesController < ApplicationController
 
     def index
         charities = Charity.all
+        render json: charities, except: [:created_at, :updated_at]
     end 
 
     def show
       charity = Charity.find_by(id: params[:id])
+      if charity
+        render json: charities, except: [:created_at, :updated_at]
+      else 
+        render json: {message: "Charity not located."}
+      end 
     end 
 
     private
