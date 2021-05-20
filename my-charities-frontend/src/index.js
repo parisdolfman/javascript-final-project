4c53f515adf841e6b430c4c3dbb6072b6eb188d9
parisdolfman <parisdolfman@gmail.com>
2021-05-20 14:29:33 -0400

    Fixed frontend to reflect objects

--- my-charities-frontend/src/index.js
+++ my-charities-frontend/src/index.js
@@ -1,6 +1,13 @@
 const BASE_URL = "http://localhost:3000"
-const CHARITIES_URL = '${BASE_URL}/charities'
-const USERS_URL = '${BASE_URL}/users'
+const CHARITIES_URL = `${BASE_URL}/charities`;
+
+const signUpForm = document.querySelector(".container")
+const addUserForm = document.querySelector(".signup-form")
+const inputFields = document.querySelectorAll(".input-text")
+const signUpBtnPhrase = document.querySelector(".sign-up-btn")
+const mainContainer = document.querySelector("main")
+const logoutBtn = document.querySelector(".logout-btn")
+const sortOptions = document.querySelector('.sort-menus')
 
 let glyphStates = {
     "♡": "♥",
@@ -25,23 +32,33 @@ for (let glyph of articleHearts) {
     glyph.addEventListener("click", likeCallback);
 }
 
-
-function fetchCharities(){
-    fetch(CHARITIES_URL)
+sortOptions.addEventListener('change', function(e){
+    fetch(BASE_URL + `/${e.target.value}`)
     .then(res => res.json())
-    .then(charities => renderCharities(charities))     
-}
+    .then(charities => renderCharities(charities))
+})
 
-function renderCharities(charities){
-    mainContainer.innerHTML = ""
-    charities.forEach(charity => {
-        mainContainer.innerHTML += `<div class="card">
-        <img src=${charity.image} class="charity-avatar" />
-        <h2>${charity.name}</h2>
-        <p><em>${charity.category}</em></p> 
-        
-        <button onClick=addToList(event) data-charity-id="${charity.id}"> Add To List </button>
-        </br>
-      </div>`
-    })
-}
+
+function fetchCharities() {
+    fetch(CHARITIES_URL)
+      .then((res) => res.json())
+      .then((charities) => renderCharities(charities));
+  }
+  
+  function renderCharities(charities) {
+    console.log(charities);
+    const mainContainer = document.getElementById('main-container');
+    charities.forEach((charity) => {
+      mainContainer.innerHTML += `<div class="card">
+          <img src=${charity.image} class="charity-avatar" />
+          <h2>${charity.name}</h2>
+          <p><em>${charity.category}</em></p>
+  
+          <button onClick=addToList(event) data-charity-id="${charity.id}"> Add To List </button>
+          </br>
+        </div>`;
+    });
+  }
+
+fetchCharities();
+
