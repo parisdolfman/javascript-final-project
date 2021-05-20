eda8702c189204933b71e6bf66937d1cb8768333
parisdolfman <parisdolfman@gmail.com>
2021-05-20 15:29:21 -0400

    Adding in user functionality

--- my-charities-backend/app/controllers/users_controller.rb
+++ my-charities-backend/app/controllers/users_controller.rb
@@ -2,7 +2,13 @@ class UsersController < ApplicationController
 
     def show
         user = User.find_by(id: params[:id])
+        render json: user, except: [:created_at, :updated_at] 
     end
+
+    def create
+        user = User.find_or_create_by(name: params[:name])
+        user.save
+    end 
 
     private
 
