dab1359133cf17e7679eb81a1b9d0e76f316eca5
parisdolfman <parisdolfman@gmail.com>
2021-05-21 13:09:06 -0400

    Adding additional fns for user list

--- my-charities-frontend/src/index.js
+++ my-charities-frontend/src/index.js
@@ -17,6 +17,11 @@ listBtn.addEventListener('mouseover', () =>{
     listContainer.style.display = "block"
 })
 
+listBtn.addEventListener('mouseout', () => {
+    listContainer.style.display = "none"
+    
+})
+
 // let glyphStates = {
 //     "♡": "♥",
 //     "♥": "♡"
@@ -71,11 +76,78 @@ function renderCharities(charities) {
 fetchCharities();
 
 function renderLoggedInUser(){
-    let welcome = document.getElementById('welcome-container');
+    let currentList = loggedIn.lists[loggedIn.lists.length - 1]
+    let welcome = document.querySelector('#welcome')
     welcome.innertText = " "
     welcome.innerText = `Welcome ${loggedIn.name}!`
+    listContainer.innerHTML = " "
+    updateQuantity()
+    for (let name in charitiesObj) {
+        let list_charity = charitiesObj[name][0]
+        listContainer.innerHTML += `<div id="listcharity-${list_charity.id}"><p> <img src=${removeIcon} onClick=removeFromList(event) data-list-charity-id="${list_charity.id}"> 
+        <img src=${addIcon} onClick=addToList(event) data-list-charity-id="${list_charity.id}" data-charity-id="${list_charity.charity.id}"> <strong>${list_charity.charity.name}</strong> </p></div>`
+    }
+    fetchCharities() 
 }
-renderLoggedInUser();
+
+function addToList(event){
+    let listId = loggedIn.carts[loggedIn.carts.length - 1].id
+    let charityCard = event.target.parentElement
+    fetch(LIST_CHARITIES_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            list_id: `${listId}`,
+            charity_id: `${event.target.dataset.charityId}`,
+        }),
+    })
+    .then(res => res.json())
+    .then(res => {
+        loggedIn = res
+        renderLoggedInUser()
+    })
+}
+
+function removeFromList(event){
+    let listCharity = event.target.dataset.listCharityId
+    fetch(CART_PLANTS_URL + "/" + cartPlant, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            id: listCharity,
+        }),
+    })
+    .then(res => res.json())
+    .then(res => {
+        loggedIn = res
+        renderLoggedInUser()
+    })
+}
+
+function checkForUser(){
+    if(localStorage.loggedIn){
+        let id = localStorage.loggedIn
+        fetch(USERS_URL + "/" + id)
+        .then(res => res.json())
+        .then(function(res){
+            loggedIn = res 
+            renderLoggedInUser()
+        })
+        hideSignUpForm();
+    } else {
+        signUpForm.style.display = "block"
+    }
+}
+
+checkForUser();
+
+
 
 signUpForm.addEventListener('submit', function(e){
     e.preventDefault()
@@ -114,8 +186,12 @@ function checkForUser(){
     }
 }
 
-
 checkForUser()
+
+logoutBtn.addEventListener('click', () => {
+    localStorage.clear(loggedIn)
+    window.location.reload()
+})
 
 
 
