004111023f4a2e739b8d3b58437dd7ac5c9a6e32
parisdolfman <parisdolfman@gmail.com>
2021-05-24 14:46:44 -0400

    Fixed add / remove functionality

--- my-charities-backend/config/routes.rb
+++ my-charities-backend/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :list_charities, only: [:index, :create, :destroy, :show]
-  resources :lists, only: [:index, :show]
   resources :charities, only: [:index, :show]
   resources :users, only: [:index, :show, :create]
+  resources :user_charities, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/charities' => 'charities#index'
